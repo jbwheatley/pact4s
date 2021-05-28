40876372a056ae289993370dd6c725390f59a5db
Kyle Florence <kyle.florence@gmail.com>
2021-05-28 18:18:55 -0500

    Issue #11: allow verification by annotated method

--- scalatest-pact/src/main/scala/pact4s/scalatest/PactVerifier.scala
+++ scalatest-pact/src/main/scala/pact4s/scalatest/PactVerifier.scala
@@ -24,7 +24,7 @@ import scala.jdk.CollectionConverters._
 
 trait PactVerifier extends AnyFlatSpec with PactVerifyResources {
   override private[pact4s] def verifySingleConsumer(consumer: IConsumerInfo): Unit =
-    it should s"Verification of ${consumer.getName}" in {
+    it should s"verify consumer '${consumer.getName}'" in {
       verifier.runVerificationForConsumer(new java.util.HashMap[String, Object](), providerInfo, consumer) match {
         case failed: VerificationResult.Failed =>
           verifier.displayFailures(List(failed).asJava)
