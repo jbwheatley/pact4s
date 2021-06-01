7bb81c81f0294f165640e69c5510cb9b3ade691e
jbwheatley <jack.wheatley@itv.com>
2021-06-01 17:46:59 +0100

    add beforeWritePacts method

--- munit-cats-effect-pact/src/main/scala/pact4s/munit/MessagePactForger.scala
+++ munit-cats-effect-pact/src/main/scala/pact4s/munit/MessagePactForger.scala
@@ -59,11 +59,20 @@ trait MessagePactForger extends CatsEffectSuite with MessagePactForgerResources 
         s"Not writing message pacts for consumer ${pact.getConsumer} and provider ${pact.getProvider} to file because tests failed."
       )
     } else {
-      pact4sLogger.info(
-        s"Writing message pacts for consumer ${pact.getConsumer} and provider ${pact.getProvider} to ${pactTestExecutionContext.getPactFolder}"
-      )
-      val write = pact.write(pactTestExecutionContext.getPactFolder, pactSpecVersion)
-      Option(write.component2()).foreach(throw _)
+      beforeWritePacts() match {
+        case Left(e) =>
+          throw e
+        case Right(_) =>
+          pact4sLogger.info(
+            s"Writing message pacts for consumer ${pact.getConsumer} and provider ${pact.getProvider} to ${pactTestExecutionContext.getPactFolder}"
+          )
+          val write = pact.write(pactTestExecutionContext.getPactFolder, pactSpecVersion)
+          Option(write.component2()).foreach(throw _)
+      }
     }
   }
+
+  type Effect[_] = Either[Throwable, _]
+
+  def beforeWritePacts(): Either[Throwable, Unit] = Right(())
 }
