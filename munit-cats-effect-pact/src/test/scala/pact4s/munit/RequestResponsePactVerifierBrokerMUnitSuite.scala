8c598f99f58b4f995531054e8cd0b82254fcb711
Kyle Florence <kyle.florence@gmail.com>
2021-06-08 20:48:21 -0500

    Clean up tests

--- munit-cats-effect-pact/src/test/scala/pact4s/munit/RequestResponsePactVerifierBrokerMUnitSuite.scala
+++ munit-cats-effect-pact/src/test/scala/pact4s/munit/RequestResponsePactVerifierBrokerMUnitSuite.scala
@@ -1,14 +1,11 @@
 package pact4s.munit
-
 import pact4s.{MockProviderServer, ProviderInfoBuilder, VerificationType}
 
-class PactVerifierMUnitSuite extends PactVerifier {
-  val mock = new MockProviderServer(2345)
+class RequestResponsePactVerifierBrokerMUnitSuite extends PactVerifier {
+  val mock = new MockProviderServer(2346)
 
-  override val provider: ProviderInfoBuilder = mock.fileSourceProviderInfo(
-    consumerName = "Pact4sConsumer",
+  override val provider: ProviderInfoBuilder = mock.brokerProviderInfo(
     providerName = "Pact4sProvider",
-    fileName = "./scripts/Pact4sConsumer-Pact4sProvider.json",
     verificationType = VerificationType.RequestResponse
   )
 
--- weaver-pact/src/test/scala/pact4s/weaver/RequestResponsePactVerifierBrokerWeaverSuite.scala
+++ weaver-pact/src/test/scala/pact4s/weaver/RequestResponsePactVerifierBrokerWeaverSuite.scala
@@ -5,17 +5,15 @@ import org.http4s.server.Server
 import pact4s.{MockProviderServer, ProviderInfoBuilder, VerificationType}
 import weaver.IOSuite
 
-object PactVerifierWeaverSuite extends IOSuite with PactVerifier[IO] {
+object RequestResponsePactVerifierBrokerWeaverSuite extends IOSuite with PactVerifier[IO] {
   type Res = Server
 
-  val mock = new MockProviderServer(1234)
+  val mock = new MockProviderServer(1235)
 
   override def sharedResource: Resource[IO, Server] = mock.server
 
-  override val provider: ProviderInfoBuilder = mock.fileSourceProviderInfo(
-    consumerName = "Pact4sConsumer",
+  override val provider: ProviderInfoBuilder = mock.brokerProviderInfo(
     providerName = "Pact4sProvider",
-    fileName = "./scripts/Pact4sConsumer-Pact4sProvider.json",
     verificationType = VerificationType.RequestResponse
   )
 
