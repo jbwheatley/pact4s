9f1bd28d994d765f65ff0bc98a49f52c3953571c
jbwheatley <jack.wheatley@itv.com>
2021-06-10 15:17:27 +0100

    refactoring suites following rebase

--- munit-cats-effect-pact/src/test/scala/pact4s/munit/RequestResponsePactVerifierBrokerMUnitSuite.scala
+++ munit-cats-effect-pact/src/test/scala/pact4s/munit/RequestResponsePactVerifierBrokerMUnitSuite.scala
@@ -1,7 +1,8 @@
 package pact4s.munit
+import munit.CatsEffectSuite
 import pact4s.{MockProviderServer, ProviderInfoBuilder, PublishVerificationResults}
 
-class RequestResponsePactVerifierBrokerMUnitSuite extends PactVerifier {
+class RequestResponsePactVerifierBrokerMUnitSuite extends CatsEffectSuite with PactVerifier {
   val mock = new MockProviderServer(2346)
 
   override val provider: ProviderInfoBuilder = mock.brokerProviderInfo("Pact4sProvider")
