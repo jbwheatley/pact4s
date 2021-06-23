527b22bc596799eaa86085d00e010b0f7776d8e0
jbwheatley <jack.wheatley@itv.com>
2021-06-23 12:00:52 +0100

    add basic support for provider states

--- scripts/publish_pacts.sh
+++ scripts/publish_pacts.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+echo "Running publish_pacts script at $SCRIPT_DIR"
+
 docker compose up
 
 export PACT_BROKER_BASE_URL="https://test.pact.dius.com.au"
@@ -5,14 +9,14 @@ export PACT_BROKER_USERNAME="dXfltyFMgNOFZAxr8io9wJ37iUpY42M"
 export PACT_BROKER_PASSWORD="O5AIZWxelWbLvqMd8PkAVycBJh2Psyg1"
 
 docker run --rm \
- -w ${PWD} \
- -v ${PWD}:${PWD} \
+ -w ${SCRIPT_DIR} \
+ -v ${SCRIPT_DIR}:${SCRIPT_DIR} \
  -e PACT_BROKER_BASE_URL \
  -e PACT_BROKER_USERNAME \
  -e PACT_BROKER_PASSWORD \
   pactfoundation/pact-cli \
   publish \
-  ${PWD} \
+  ${SCRIPT_DIR} \
   --consumer-app-version fake-git-sha-for-demo-$(date +%s) \
 
 docker compose down
