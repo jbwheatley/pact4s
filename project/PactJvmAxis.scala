4b464a0c8271d9523e9b3875049f9ee1dad2e3cc
jbwheatley <jack.wheatley@itv.com>
2021-07-01 15:55:10 +0100

    trying to make project matrix work

---
+++ project/PactJvmAxis.scala
@@ -0,0 +1,12 @@
+import sbt.VirtualAxis
+
+case class PactJvmAxis(series: String, version: String) extends VirtualAxis.WeakAxis {
+  val directorySuffix      = series
+  val idSuffix             = series
+  override val suffixOrder = 50
+}
+
+object PactJvmAxis {
+  lazy val java11 = PactJvmAxis("", Dependencies.pactJvmJava11)
+  lazy val java8  = PactJvmAxis("-java8", Dependencies.pactJvmJava8)
+}
