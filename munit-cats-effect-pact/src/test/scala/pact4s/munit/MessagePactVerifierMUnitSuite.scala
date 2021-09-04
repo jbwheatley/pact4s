9dbd86e21034b3e4742ea01f7cc0bbe529014d96
jbwheatley <19939655+jbwheatley@users.noreply.github.com>
2021-09-05 00:23:28 +0100

    only assign port for requestResponse pacts

--- munit-cats-effect-pact/src/test/scala/pact4s/munit/MessagePactVerifierMUnitSuite.scala
+++ munit-cats-effect-pact/src/test/scala/pact4s/munit/MessagePactVerifierMUnitSuite.scala
@@ -5,7 +5,7 @@ import pact4s.{MockProviderServer, ProviderInfoBuilder}
 import pact4s.messages.MessagesProvider
 
 class MessagePactVerifierMUnitSuite extends CatsEffectSuite with MessagePactVerifier {
-  val mock = new MockProviderServer
+  val mock = new MockProviderServer(isRequestResponse = false)
 
   def messages: ResponseFactory = MessagesProvider.messages
   override val provider: ProviderInfoBuilder = mock.fileSourceProviderInfo(
