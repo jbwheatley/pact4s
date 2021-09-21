f1bdc4c045ae34612c726e8b82b5298b06539dbc
Scala Steward <me@scala-steward.org>
2021-09-21 16:56:59 +0200

    Update http4s-circe, http4s-dsl, ... to 1.0.0-M27

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   val mockitoScala     = "1.16.42"
   val pactJvmJava11    = "4.2.11"
   val pactJvmJava8     = "4.1.26"
-  val http4s           = "1.0.0-M25"
+  val http4s           = "1.0.0-M27"
   val log4s            = "1.10.0"
   val logback          = "1.2.6"
   val collectionCompat = "2.5.0"
