b0036f6d589b2094f3551915f392a8912f7fb675
jbwheatley <jackbwheatley@gmail.com>
2021-09-23 15:54:54 +0100

    Update Dependencies.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,8 +3,6 @@ import sbt._
 object Dependencies {
   val mockitoScala     = "1.16.42"
   val pactJvmJava11    = "4.2.11"
-  val pactJvmJava8     = "4.1.26"
-  val http4s           = "1.0.0-M27"
   val pactJvmJava8     = "4.1.27"
   val http4s           = "1.0.0-M27"
   val log4s            = "1.10.0"
