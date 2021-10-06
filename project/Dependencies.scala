1dc4c5f2e9c8b916a75bd937da6955cae3038705
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-10-06 13:26:55 +0200

    Update consumer to 4.2.14 (#85)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   val mockitoScala     = "1.16.42"
-  val pactJvmJava11    = "4.2.13"
+  val pactJvmJava11    = "4.2.14"
   val pactJvmJava8     = "4.1.28"
   val http4s           = "1.0.0-M27"
   val log4s            = "1.10.0"
