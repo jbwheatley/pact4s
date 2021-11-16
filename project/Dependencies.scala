56d88f21a8cb54463c9ffdfdcfef371d1c0a2fad
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-11-16 12:29:59 +0100

    Update logback-classic to 1.2.7 (#105)
    Co-authored-by: jbwheatley <jackbwheatley@gmail.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   val pactJvmJava8     = "4.1.28"
   val http4s           = "1.0.0-M29"
   val log4s            = "1.10.0"
-  val logback          = "1.2.6"
+  val logback          = "1.2.7"
   val collectionCompat = "2.6.0"
   val sourcecode       = "0.2.7"
   val _circe           = "0.14.1"
