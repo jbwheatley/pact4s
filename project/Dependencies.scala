9d3dd37192ed108c08203a78858783152558fc32
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-11-16 12:29:30 +0100

    Update scala-collection-compat to 2.6.0 (#103)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val http4s           = "1.0.0-M29"
   val log4s            = "1.10.0"
   val logback          = "1.2.6"
-  val collectionCompat = "2.5.0"
+  val collectionCompat = "2.6.0"
   val sourcecode       = "0.2.7"
   val _circe           = "0.14.1"
   val _weaver          = "0.7.7"
