42c8dfcd19ccb54a7877fd3f70e0c26d5ce3fb13
Scala Steward <me@scala-steward.org>
2021-12-09 12:42:55 +0100

    Update consumer to 4.1.31

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   val mockitoScala     = "1.16.46"
   val pactJvmJava11    = "4.2.15"
-  val pactJvmJava8     = "4.1.29"
+  val pactJvmJava8     = "4.1.31"
   val http4s           = "1.0.0-M29"
   val log4s            = "1.10.0"
   val logback          = "1.2.7"
