c0051926ed3706ddb6fc192ab1faececc850d376
Jack Wheatley <jackbwheatley@gmail.com>
2022-02-24 17:30:54 +0000

    add tests for feature branch verification

--- scripts/publish_pacts.sh
+++ scripts/publish_pacts.sh
@@ -2,8 +2,6 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 echo "Running publish_pacts script at $SCRIPT_DIR"
 
-docker compose up
-
 export PACT_BROKER_BASE_URL="https://test.pact.dius.com.au"
 export PACT_BROKER_USERNAME="dXfltyFMgNOFZAxr8io9wJ37iUpY42M"
 export PACT_BROKER_PASSWORD="O5AIZWxelWbLvqMd8PkAVycBJh2Psyg1"
@@ -18,5 +16,16 @@ docker run --rm \
   publish \
   ${SCRIPT_DIR} \
   --consumer-app-version fake-git-sha-for-demo-$(date +%s) \
+  --branch main
 
-docker compose down
+docker run --rm \
+ -w "${SCRIPT_DIR}/feature" \
+ -v "${SCRIPT_DIR}/feature":"${SCRIPT_DIR}/feature" \
+ -e PACT_BROKER_BASE_URL \
+ -e PACT_BROKER_USERNAME \
+ -e PACT_BROKER_PASSWORD \
+  pactfoundation/pact-cli \
+  publish \
+  "${SCRIPT_DIR}/feature" \
+  --consumer-app-version fake-git-sha-for-demo-$(date +%s) \
+  --branch feat/x
--- weaver-pact/src/test/scala/pact4s/weaver/RequestResponsePactVerifierBrokerWeaverSuite.scala
+++ weaver-pact/src/test/scala/pact4s/weaver/RequestResponsePactVerifierBrokerWeaverSuite.scala
@@ -3,7 +3,7 @@ package pact4s.weaver
 import cats.effect.{IO, Resource}
 import org.http4s.server.Server
 import pact4s.MockProviderServer
-import pact4s.provider.{ProviderInfoBuilder, PublishVerificationResults}
+import pact4s.provider.{ConsumerVersionSelector, ProviderInfoBuilder, PublishVerificationResults}
 import weaver.IOSuite
 
 object RequestResponsePactVerifierBrokerWeaverSuite extends IOSuite with PactVerifier {
@@ -13,17 +13,50 @@ object RequestResponsePactVerifierBrokerWeaverSuite extends IOSuite with PactVer
 
   override def sharedResource: Resource[IO, Server] = mock.server
 
-  override val provider: ProviderInfoBuilder = mock.brokerProviderInfo("Pact4sProvider")
+  override val provider: ProviderInfoBuilder =
+    mock.brokerProviderInfo("Pact4sProvider", consumerVersionSelector = ConsumerVersionSelector().withMainBranch(true))
 
-  pureTest("Verify pacts for provider `Pact4sProvider`") {
-    succeed(
-      verifyPacts(
-        publishVerificationResults = Some(
-          PublishVerificationResults(
-            providerVersion = "SNAPSHOT"
+  test("Verify pacts for provider `Pact4sProvider`") {
+    for {
+      a <- IO(
+        succeed(
+          verifyPacts(
+            publishVerificationResults = Some(
+              PublishVerificationResults(
+                providerVersion = "SNAPSHOT"
+              )
+            )
           )
         )
       )
-    )
+      x <- mock.featureXState.tryGet
+    } yield a && assert(x.isEmpty)
+  }
+}
+object RequestResponsePactVerifierBrokerFeatureBranchWeaverSuite extends IOSuite with PactVerifier {
+  type Res = Server
+
+  val mock = new MockProviderServer(49173)
+
+  override def sharedResource: Resource[IO, Server] = mock.server
+
+  override val provider: ProviderInfoBuilder =
+    mock.brokerProviderInfo("Pact4sProvider", consumerVersionSelector = ConsumerVersionSelector().withBranch("feat/x"))
+
+  test("Verify pacts for provider `Pact4sProvider`") {
+    for {
+      a <- IO(
+        succeed(
+          verifyPacts(
+            publishVerificationResults = Some(
+              PublishVerificationResults(
+                providerVersion = "SNAPSHOT"
+              )
+            )
+          )
+        )
+      )
+      x <- mock.featureXState.tryGet
+    } yield a && assert(x.contains(true))
   }
 }
