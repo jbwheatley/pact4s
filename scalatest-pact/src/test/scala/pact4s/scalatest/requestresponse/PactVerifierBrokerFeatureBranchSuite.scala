6b5d23f98fb2870efa4cbba21c9a2a5b85c1b7a4
jbwheatley <19939655+jbwheatley@users.noreply.github.com>
2022-02-24 19:53:06 +0000

    matching branch selector test

--- scalatest-pact/src/test/scala/pact4s/scalatest/requestresponse/PactVerifierBrokerFeatureBranchSuite.scala
+++ scalatest-pact/src/test/scala/pact4s/scalatest/requestresponse/PactVerifierBrokerFeatureBranchSuite.scala
@@ -6,15 +6,17 @@ import org.scalatest.BeforeAndAfterAll
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import pact4s.MockProviderServer
-import pact4s.provider.{ConsumerVersionSelector, ProviderInfoBuilder}
+import pact4s.provider.{Branch, ConsumerVersionSelector, ProviderInfoBuilder, PublishVerificationResults}
 import pact4s.scalatest.PactVerifier
+
+import scala.concurrent.duration._
 
 class PactVerifierBrokerFeatureBranchSuite
     extends AnyFlatSpec
     with PactVerifier
     with BeforeAndAfterAll
     with Matchers {
-  val mock = new MockProviderServer(49168, hasFeatureX = true)
+  val mock = new MockProviderServer(49200, hasFeatureX = true)
 
   override val provider: ProviderInfoBuilder =
     mock.brokerProviderInfo("Pact4sProvider", consumerVersionSelector = ConsumerVersionSelector().withBranch("feat/x"))
@@ -29,8 +31,15 @@ class PactVerifierBrokerFeatureBranchSuite
   override def afterAll(): Unit =
     cleanUp.unsafeRunSync()
 
-  it should "Verify pacts for provider `Pact4sProvider`" in {
-    verifyPacts()
-    mock.featureXState.tryGet.unsafeRunSync() shouldBe Some(true)
+  it should "Verify pacts for provider `Pact4sProvider` with a feature branch, scalatest" in {
+    verifyPacts(
+      publishVerificationResults = Some(
+        PublishVerificationResults(
+          providerVersion = "SNAPSHOT",
+          providerBranch = Branch("feat/x")
+        )
+      )
+    )
+    mock.featureXState.get.unsafeRunTimed(10.seconds) shouldBe Some(true)
   }
 }
